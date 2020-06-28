3459d5fb3f1a43476def30e1938e4eb28c6f0271
ADAM NICHOLS <ANichols4@protonmail.com>
2020-06-28 15:04:05 -0400

    updated login html with new thymeleaf references

--- looksmart-webapp/src/main/java/club/looksmart/looksmartwebapp/api/LoginController.java
+++ looksmart-webapp/src/main/java/club/looksmart/looksmartwebapp/api/LoginController.java
@@ -3,7 +3,9 @@ package club.looksmart.looksmartwebapp.api;
 import club.looksmart.looksmartwebapp.model.Login;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -14,8 +16,7 @@ public class LoginController {
         return "student_login";
     }
 
-    @RequestMapping(value = "/student_login",
-            method= RequestMethod.PUT)
+    @PostMapping("/student_login")
     public String loginSubmit(@ModelAttribute Login login)
     {
         return "reservations";
--- looksmart-webapp/src/main/java/club/looksmart/looksmartwebapp/model/Login.java
+++ looksmart-webapp/src/main/java/club/looksmart/looksmartwebapp/model/Login.java
@@ -3,10 +3,10 @@ package club.looksmart.looksmartwebapp.model;
 public class Login {
 
     private String password;
-    private String username;
+    private String email;
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public void setPassword(String password) {
