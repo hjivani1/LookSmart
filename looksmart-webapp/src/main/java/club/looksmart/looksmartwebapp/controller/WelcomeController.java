8fe7a3fb213dac0f77698aeba3214a6949c4b80d
ADAM NICHOLS <ANichols4@protonmail.com>
2020-06-28 17:08:30 -0400

    added WebController

--- looksmart-webapp/src/main/java/club/looksmart/looksmartwebapp/controller/WelcomeController.java
+++ looksmart-webapp/src/main/java/club/looksmart/looksmartwebapp/controller/WelcomeController.java
@@ -1,8 +1,10 @@
 package club.looksmart.looksmartwebapp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class WelcomeController {
 
     @GetMapping("/")
